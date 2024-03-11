cdda016687e9ef6673c04b934b41a7f28264b95a
Peniel Bekele <142409326+Penie1@users.noreply.github.com>
2024-03-11 16:42:49 +0300

    attach click event handler to the navbar

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 "use strict";
 
 const yearsEl = document.getElementById("years");
+const headerEl = document.getElementById("header");
+const navbar = document.getElementById("nav-bar");
+
 const date = new Date();
 const years = date.getFullYear();
 yearsEl.innerText = years;
+
+navbar.addEventListener("click", (e) => {
+  e.preventDefault();
+});
